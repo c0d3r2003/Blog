2f8f44f648ad160368683b6156770821cbcae865
mahmoud <hvji81@gmail.com>
2024-01-08 12:35:16 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MainPage());
+
+
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Blog Club',
+      home: Material(),
+    );
+  }
+}
